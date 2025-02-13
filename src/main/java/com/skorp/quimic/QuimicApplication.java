f23db3628f03983189479c3636688732db8294fb
AlexCont2001 <rodrigoalexcontreras@gmail.com>
2025-02-12 21:08:07 -0300

    initial-commit

---
+++ src/main/java/com/skorp/quimic/QuimicApplication.java
@@ -0,0 +1,13 @@
+package com.skorp.quimic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuimicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuimicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skorp/quimic/QuimicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skorp.quimic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuimicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
