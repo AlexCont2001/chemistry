522f7bffa938a4ad1bb3443653263d6d91423495
AlexCont2001 <rodrigoalexcontreras@gmail.com>
2025-02-13 16:42:41 -0300

    change response type and create getbyid()

--- src/main/java/com/skorp/chemistry/controllers/ElementCategoriesController.java
+++ src/main/java/com/skorp/chemistry/controllers/ElementCategoriesController.java
@@ -3,11 +3,14 @@ package com.skorp.chemistry.controllers;
 import com.skorp.chemistry.models.ElementCategories;
 import com.skorp.chemistry.services.ElementCategoriesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/element_categories")
@@ -21,8 +24,13 @@ public class ElementCategoriesController {
     }
 
     @GetMapping
-    public ArrayList<ElementCategories> GetAllElementCategories(){
+    public ResponseEntity<ArrayList<ElementCategories>> GetAllElementCategories(){
         return  elementCategoriesService.GetAllElementCategories();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<ElementCategories>> GetElementCategoryById(@PathVariable int id) {
+        return elementCategoriesService.GetElementCategoryById(id);
     }
 }
 
--- src/main/java/com/skorp/chemistry/services/ElementCategoriesService.java
+++ src/main/java/com/skorp/chemistry/services/ElementCategoriesService.java
@@ -3,9 +3,12 @@ package com.skorp.chemistry.services;
 import com.skorp.chemistry.models.ElementCategories;
 import com.skorp.chemistry.repositories.IElementCategoriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class ElementCategoriesService {
@@ -17,7 +20,24 @@ public class ElementCategoriesService {
         this.elementCategoriesRepository = elementCategoriesRepository;
     }
 
-    public ArrayList<ElementCategories> GetAllElementCategories(){
-        return (ArrayList<ElementCategories>) elementCategoriesRepository.findAll();
+    public ResponseEntity<ArrayList<ElementCategories>> GetAllElementCategories(){
+        try{
+            ArrayList<ElementCategories> elements = (ArrayList<ElementCategories>) elementCategoriesRepository.findAll();
+            return  ResponseEntity.status(HttpStatus.OK).body(elements);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ArrayList<>());
+        }
+    }
+
+    public ResponseEntity<Optional<ElementCategories>> GetElementCategoryById(int id){
+        try{
+            Optional<ElementCategories> elementCategory = elementCategoriesRepository.findById(id);
+            if(elementCategory.isPresent()){
+                return ResponseEntity.status(HttpStatus.OK).body(elementCategory);
+            }
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Optional.empty());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Optional.empty());
+        }
     }
 }
